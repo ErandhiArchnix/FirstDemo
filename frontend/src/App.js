03137061d073318eb5f058bcfce2bf41cceb1e53
ErandhiArchnix <erandhi.madhushika@archnix.com>
2024-03-12 14:55:33 +0530

    Create Signup Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 import Signup from "./pages/signup";
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <ToastContainer />
         <Routes>
           <Route path="/signup" element={<Signup />} />
         </Routes>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -40,22 +40,22 @@ const AuthReducer = (state, action) => {
 };
 
 export const AuthContextProvider = ({ children }) =>{
-    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-    useEffect(()=>{
-        localStorage.setItem("user", JSON.stringify(state.user))
-    },[state.user])
+  useEffect(()=>{
+      localStorage.setItem("user", JSON.stringify(state.user))
+  },[state.user])
 
-    return(
-        <AuthContext.Provider
-            value={{
-                user: state.user,
-                loading:state.loading,
-                error: state.error,
-                dispatch,
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
+  return(
+      <AuthContext.Provider
+          value={{
+              user: state.user,
+              loading:state.loading,
+              error: state.error,
+              dispatch,
+          }}
+      >
+          {children}
+      </AuthContext.Provider>
+  )
 }
